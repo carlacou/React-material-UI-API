f06441c527087c0124b13d28290f26bcef971321
carlacou <carlacoutinhopontes@gmail.com>
2022-02-10 19:03:44 -0300

    footer-responsivo

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -7,50 +7,122 @@ import { Grid } from '@mui/material';
 const footer = () => {
   return (
     <footer>
-      <Grid sx={{mb:5, mt:5, ml:2, background:'lightgray'}}  container spacing={2}>
-      <Grid item md={3}>
-        <Box sx={{ width: '30%' }} component='img'
-          src='images/payment/paypal.png'
-        />
+      <Grid sx={{ mb: 5, mt: 5, ml: 2, background: 'lightgray' }} container spacing={2}>
+        <Grid sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            lg: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        }}
+          item md={3}>
+          <Box sx={{ width: '30%' }} component='img'
+            src='images/payment/paypal.png'
+          />
         </Grid>
-        <Grid item md={3}>
-        <Box sx={{ width: '30%' }} component='img'
-          src='images/payment/paykeeper.png'
-        />
+        <Grid sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            lg: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        }}
+          item md={3}>
+          <Box sx={{ width: '30%' }} component='img'
+            src='images/payment/paykeeper.png'
+          />
         </Grid>
-        <Grid item md={3}>
-        <Box sx={{ width: '30%' }} component='img'
-          src='images/payment/visa.png'
-        />
+        <Grid sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            lg: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        }}
+          item md={3}>
+          <Box sx={{ width: '30%' }} component='img'
+            src='images/payment/visa.png'
+          />
         </Grid>
-        <Grid item md={3}>
-        <Box sx={{ width: '30%' }} component='img'
-          src='images/payment/googlePay.png'
-        />
+        <Grid sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            lg: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        }}
+          item md={3}>
+          <Box sx={{ width: '30%' }} component='img'
+            src='images/payment/googlePay.png'
+          />
         </Grid>
-        <Grid item md={3}>
-        <Box sx={{ width: '30%' }} component='img'
-          src='images/payment/yandex.png'
-        />
+        <Grid sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            lg: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        }}
+          item md={3}>
+          <Box sx={{ width: '30%' }} component='img'
+            src='images/payment/yandex.png'
+          />
         </Grid>
-        <Grid item md={3}>
-        <Box sx={{ width: '30%' }} component='img'
-          src='images/payment/maestro.png'
-        />
+        <Grid sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            lg: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        }}
+          item md={3}>
+          <Box sx={{ width: '30%' }} component='img'
+            src='images/payment/maestro.png'
+          />
         </Grid>
-        <Grid item md={3}>
-        <Box sx={{ width: '30%' }} component='img'
-          src='images/payment/ruble.png'
-        />
+        <Grid sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            lg: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        }}
+          item md={3}>
+          <Box sx={{ width: '30%' }} component='img'
+            src='images/payment/ruble.png'
+          />
         </Grid>
-        <Grid item md={3}>
-        <Box sx={{ width: '30%' }} component='img'
-          src='images/payment/wester union.png'
-        />
+        <Grid sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            lg: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+        }}
+          item md={3}>
+          <Box sx={{ width: '30%' }} component='img'
+            src='images/payment/wester union.png'
+          />
         </Grid>
-        </Grid>
-        
-        </footer>
+      </Grid>
+
+    </footer>
   )
 }
 
